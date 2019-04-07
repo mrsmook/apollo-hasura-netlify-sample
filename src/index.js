d8696ef1ce76f3465b1e330a26de4d87aebfb3ea
zaouali.mohamed.selim@gmail.com <zaouali.mohamed.selim@gmail.com>
2019-04-07 20:10:40 +0200

    new domain graph

--- src/index.js
+++ src/index.js
@@ -11,10 +11,10 @@ import registerServiceWorker from './registerServiceWorker';
 
 const client = new ApolloClient({
   link: new HttpLink({ uri: process.env.REACT_APP_API_URL }),
-  cache: new InMemoryCache(),
   headers: {
-        x-hasura-admin-secret: BWePfsaTdHGBgcU
-    }
+        'x-hasura-admin-secret': BWePfsaTdHGBgcU
+    },
+  cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
